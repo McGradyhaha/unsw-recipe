cd518238eea92113629a7403f67d0553661037d0
WhyK <mcgradyhaha@gmail.com>
2018-08-15 22:59:21 +1000

    design folder updated

--- Code/config-overrides.js
+++ Code/config-overrides.js
@@ -5,3 +5,4 @@ module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
     return config;
 };
+
